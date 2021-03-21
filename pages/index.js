b07ffa5f7c1b7699ee489f07286fd611380aab19
jai-sharma <sendtojsharma@gmail.com>
2021-03-21 08:49:07 +0530

    fixed formatting

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Jsonplaceholder api viewer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Jsonplaceholder api viewer</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-     </>
-  )
+            
+        </>
+    );
 }
